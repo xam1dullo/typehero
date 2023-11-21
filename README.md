f45e8ca4de41a46eb31b64e805a3a1c116f2dc6e
khamidullo <khkhamidullo@gmail.com>
2023-11-21 21:38:58 +0500

    add TypeScript Cheat Sheets

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # typehero
-typescript learning challenges
+
+## Typescript learning challenges
+
+1 . [Generic Function Arguments](https://github.com/xam1dullo/typehero/blob/main/Generic%20Function%20Arguments/task.md)
---
+++ test.sh
@@ -0,0 +1,49 @@
+  #!/usr/bin/python
+      
+
+
+  print("Welcome")
+  print("to")
+  print("quickref.me")
+  print("/vim")
+
+
+
+
+
+
+
+
+
+
+
+  print("Hello world")
+
+  print("Hello world")
+  print("Hello world")
+  print("Hello world")
+  print("Hello world")
+  print("Hello world")
+  print("Hello world")
+  print("Hello world")
+  print("Hello world")
+  print("Hello world")
+  print("Hello world")
+  print("Hello world")
+  print("Hello world")
+  print("Hello world")
+
+
+  print("Hello world")
+
+
+
+
+
+
+
+
+
+
+
+

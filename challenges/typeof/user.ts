8e5d6c000eb79a86b86033e9060426fc7a689a8b
khamidullo <khkhamidullo@gmail.com>
2023-11-28 21:33:24 +0500

    The keyof operator done

--- challenges/typeof/user.ts
+++
@@ -1,6 +0,0 @@
-type Width = typeof width;
-type Margin = typeof margin;
-type Data = typeof d3ChartConfig.data;
-type YScale = typeof d3ChartConfig.yScale;
-
-type D3ChartConfig = typeof d3ChartConfig;
